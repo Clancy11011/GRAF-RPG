22bde905c884830079374ed26909e459b4d07e35
ThePrincePolo <ThePrincePolo09@gmail.com>
2024-05-06 18:29:10 -0400

    Update guard.gd
    Commented out both if statements

--- Characters/Guard/guard.gd
+++ Characters/Guard/guard.gd
@@ -4,10 +4,10 @@ class_name Guard
 func _physics_process(delta):
 	move_and_slide()
 
-	if velocity.length() > 0:
-		$AnimationPlayer.play("Walk")
+	#if velocity.length() > 0:
+	#	$AnimationPlayer.play("Walk")
 	
-	if velocity.x > 0:
-		$Sprite.flip_h = false
-	else:
-		$Sprite.flip_h = true
+	#if velocity.x > 0:
+	#	$Sprite.flip_h = false
+	#else:
+	#	$Sprite.flip_h = true
