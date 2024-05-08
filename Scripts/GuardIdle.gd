0b631f5b812e85593395ae3973328d016037d3ae
ThePrincePolo <ThePrincePolo09@gmail.com>
2024-05-08 00:15:32 -0400

    Fine tuned guard movement
    guard will only move on the x and y axis and randomly change directions

--- Scripts/GuardIdle.gd
+++ Scripts/GuardIdle.gd
@@ -9,19 +9,35 @@ var player: CharacterBody2D
 var move_direction : Vector2
 var wander_time : float
 
-func randomize_wander():
-	move_direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
-	wander_time = randf_range(1, 3)
-	
+@export var current_states = enemy_states.MOVEDOWN
+enum enemy_states{MOVERIGHT, MOVELEFT, MOVEUP, MOVEDOWN}
+
+var dir
+
 func enter():
+	print("enter enter")
 	player = get_tree().get_first_node_in_group("Player")
 	randomize_wander()
-	
-func update(delta: float):
-	if wander_time > 0:
-		wander_time -= delta
-	else:
-		randomize_wander()
+
+func randomize_wander():
+	#move_direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
+	#wander_time = randf_range(1, 3)
+	print("enter random wander")
+	match current_states:
+		enemy_states.MOVERIGHT:
+			move_right()
+		enemy_states.MOVELEFT:
+			move_left()
+		enemy_states.MOVEUP:
+			move_up()
+		enemy_states.MOVEDOWN:
+			move_down()
+
+#func update(delta: float):
+#	if wander_time > 0:
+#		wander_time -= delta
+#	else:
+#		randomize_wander()
 
 func Physics_Update(delta: float):
 	if enemy:
@@ -31,3 +47,36 @@ func Physics_Update(delta: float):
 	
 	if direction.length() < 100 && move_direction.angle_to(player.global_position) < PI/4:
 		Transitioned.emit(self, "Follow")
+
+func move_right():
+	move_direction = Vector2.RIGHT
+
+func move_left():
+	move_direction = Vector2.LEFT
+
+func move_up():
+	move_direction = Vector2.UP
+
+func move_down():
+	move_direction = Vector2.DOWN
+
+func random_generation():
+	dir = randi() % 4
+	print(dir)
+	random_direction()
+	
+func random_direction():
+	match dir:
+		0:
+			current_states = enemy_states.MOVERIGHT
+		1:
+			current_states = enemy_states.MOVELEFT
+		2:
+			current_states = enemy_states.MOVEUP
+		3:
+			current_states = enemy_states.MOVEDOWN
+
+func _on_timer_timeout():
+	random_generation()
+	$Timer.start()
+	randomize_wander()
--- guard.gd
+++ guard.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 class_name GuardEnemy
 
-#@export var move_speed : float = 10
+@export var move_speed : float = 10
 @export var start_direction : Vector2 = Vector2(0, 1)
 
 @onready var animation_tree = $AnimationTree
@@ -11,27 +11,17 @@ func _ready():
 	update_animation_param(start_direction)
 
 func _physics_process(delta):
-	
 	update_animation_param(velocity * Vector2(1, -1))
-	
+
 	move_and_slide()
-	
 	change_state()
-	
-	#if velocity.length() > 0:
-	#	$AnimationPlayer.play("walk_down")
-		
-	#if velocity.x > 0:
-	#	$Sprite2D.flip_h = false
-	#else:
-	#	$Sprite2D.flip_h = true
+
 
 func update_animation_param(velocity : Vector2):
-	
 	if (velocity != Vector2.ZERO):
 		animation_tree.set("parameters/Walk/blend_position", velocity)
 		animation_tree.set("parameters/Idle/blend_position", velocity)
-		
+
 func change_state():
 	if (velocity != Vector2.ZERO):
 		state_machine.travel("Walk")
