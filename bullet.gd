896173f1fc930ac85fef42ec8a3e5caf81e17acd
Tyler Forrest <tylerwforrest@gmail.com>
2024-06-18 17:01:44 -0400

    updated bullet script

---
+++ bullet.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+var distance_travelled = 0
+
+func _physics_process(delta):
+	const SPEED = 200
+	const RANGE = 1200
+	
+	var direction = Vector2.RIGHT.rotated(rotation)
+	position += direction * SPEED * delta
+	
+	distance_travelled += SPEED * delta
+	if distance_travelled > RANGE:
+		queue_free()
+
+
+func _on_body_entered(body):
+	pass # Replace with function body.
